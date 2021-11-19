e8fe751ad0679ec23017f8d2810e50ef009f893f
Derek Linden <dlinden2024@chadwickschool.org>
2021-11-19 08:39:10 -0800

    connected start screen and play screen

--- Snowman/Assets/GameController.cs
+++ Snowman/Assets/GameController.cs
@@ -6,18 +6,26 @@ public class GameController : MonoBehaviour
 {
     public UnityEngine.UI.Text Message;
     public UnityEngine.UI.Button StartButton;
-
+    public GameObject StartScreen;
+    public GameObject PlayScreen;
+    
     public void StartGame()
     {
         this.Message.text = "Can you save the Snowman?";
         this.StartButton.gameObject.SetActive(false);
-        this.StartGame.SetActive(false);
+        this.StartScreen.SetActive(false);
         this.PlayScreen.SetActive(true);
     }
 
     public void OpenStartScreen()
     {
-        this.StartGame.gameObject.SetActive(true);
         this.PlayScreen.SetActive(false);
+        this.StartScreen.SetActive(true);
+    }
+
+    public void Start()
+    {
+      this.StartScreen.SetActive(true);
+      this.PlayScreen.SetActive(false);  
     }
 }
