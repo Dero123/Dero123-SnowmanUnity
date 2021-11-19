83c26bf5d3279584264330fdcf901de88bc00efc
Derek Linden <dlinden2024@chadwickschool.org>
2021-11-19 09:05:08 -0800

    added code

--- Snowman/Assets/GameController.cs
+++ Snowman/Assets/GameController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WordGuesser;
 
 public class GameController : MonoBehaviour
 {
@@ -8,9 +9,14 @@ public class GameController : MonoBehaviour
     public UnityEngine.UI.Button StartButton;
     public GameObject StartScreen;
     public GameObject PlayScreen;
-    
+    private WordGame guessingGame;
     public void StartGame()
     {
+        this.guessingGame = new WordGuesser.WordGame("apple", 5);
+        Debug.Log(this.guessingGame.GetWord());
+        Debug.Log(this.guessingGame.GetFullWord());
+
+
         this.Message.text = "Can you save the Snowman?";
         this.StartButton.gameObject.SetActive(false);
         this.StartScreen.SetActive(false);
