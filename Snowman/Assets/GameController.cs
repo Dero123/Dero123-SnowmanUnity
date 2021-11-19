c2de7870cddbce9c715afc2b656a9893e6d8d5d2
Derek Linden <dlinden2024@chadwickschool.org>
2021-11-18 21:09:07 -0800

    Added new background and code

--- Snowman/Assets/GameController.cs
+++ Snowman/Assets/GameController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public UnityEngine.UI.Text Message;
+    public UnityEngine.UI.Button StartButton;
+
+    public void StartGame()
     {
-        
+        this.Message.text = "Can you save the Snowman?";
+        this.StartButton.gameObject.SetActive(false);
+        this.StartGame.SetActive(false);
+        this.PlayScreen.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenStartScreen()
     {
-        
+        this.StartGame.gameObject.SetActive(true);
+        this.PlayScreen.SetActive(false);
     }
 }
